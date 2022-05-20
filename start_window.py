b55b1c47e778b7e446df03fe3af5405787d0f668
Keijo Antero Linnamaa <keijo.linnamaa@tite.amiedu-ict.net>
2022-05-20 10:05:21 +0300

    windows update

--- start_window.py
+++ start_window.py
@@ -1,23 +1,25 @@
 
 import tkinter as tk
-from tkinter import ttk, BOTH
+from tkinter import BOTH, END
 from tkinter.messagebox import showinfo
+from client_window import ClientWindow
+from admin_window import AdminWindow
 
-class StartWindow(tk.Tk):
-    def __init__(self):
-        super().__init__()
+class StartWindow():
+    def __init__(self):  
         
-        # configure the root window
-        self.title('Start Window')
-        self.iconbitmap('icons/rat.ico')
-        self.geometry('600x500+500+200')
-        self.config(bg='black')
-        self.resizable(0,0)
+        # configure the start window
+        self.start = tk.Tk()
+        self.start.title('Start Window')
+        self.start.iconbitmap('icons/rat.ico')
+        self.start.geometry('600x500+500+200')
+        self.start.config(bg='black')
+        self.start.resizable(0,0)
 
         # setting layout frames
-        self.header_frame = tk.Frame(self, width=600, height=100, bg='black')
-        self.midle_frame = tk.Frame(self, width=600, height=350, bg='black', highlightbackground='lightgreen', highlightcolor='lightgreen', highlightthickness=2, bd=0)
-        self.footer_frame = tk.Frame(self, width=600, height=50, bg='black')
+        self.header_frame = tk.Frame(self.start, width=600, height=100, bg='black')
+        self.midle_frame = tk.Frame(self.start, width=600, height=350, bg='black', highlightbackground='lightgreen', highlightcolor='lightgreen', highlightthickness=2, bd=0)
+        self.footer_frame = tk.Frame(self.start, width=600, height=50, bg='black')
         self.header_frame.pack()
         self.midle_frame.pack(padx = 25, pady= (20,25), fill=BOTH)
         self.footer_frame.pack()
@@ -32,11 +34,11 @@ class StartWindow(tk.Tk):
         # midle labels and buttons
         self.midle_label_1 = tk.Label(self.midle_frame, text='Run as:', bg='black', fg='white', font=('helvetica', 15))
         self.midle_label_1.pack(pady=10)
-        self.midle_button_client = tk.Button(self.midle_frame, text='Client', fg='black', bg='lightgreen', font=('helvetica', 15))
+        self.midle_button_client = tk.Button(self.midle_frame, text='Client', fg='black', bg='lightgreen', font=('helvetica', 15), command=self.open_client)
         self.midle_button_client.pack(pady=10)
         self.midle_label_2 = tk.Label(self.midle_frame, text='or', bg='black', fg='white', font=('helvetica', 15))
         self.midle_label_2.pack(pady=10)
-        self.midle_button_admin = tk.Button(self.midle_frame, text='Admin', fg='black', bg='orange', font=('helvetica', 15))
+        self.midle_button_admin = tk.Button(self.midle_frame, text='Admin', fg='black', bg='orange', font=('helvetica', 15), command=self.open_admin)
         self.midle_button_admin.pack(pady=10)
         self.midle_label_3 = tk.Label(self.midle_frame, text='If running as admin, please enter password:', bg='black', fg='white', font=('helvetica', 15))
         self.midle_label_3.pack(pady=10)
@@ -47,9 +49,28 @@ class StartWindow(tk.Tk):
         self.footer_label_1 = tk.Label(self.footer_frame, text='by Rat Masters', bg='black', fg='white', font=('helvetica', 10))
         self.footer_label_1.pack()
         
+        self.start.mainloop()
+
+    #Button functions
+    def open_client(self):
+        self.start.destroy()        
+        self.client_window = ClientWindow()
+        
+
+    def open_admin(self):        
+        if self.midle_entry.get() != "rotta":
+            showinfo(title="NOT CORRECT!", message="Please enter correct  password!")
+            self.midle_entry.delete(0,END)
+        else:
+            self.start.destroy()           
+            self.admin_window = AdminWindow()
+            
 
 if __name__ == "__main__":
-    app = StartWindow() 
-    app.mainloop()
+    app = StartWindow()
+    
+
+        
+
 
  
